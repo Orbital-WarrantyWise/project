f5a137dbbb4e813b4dfa3a8ba18d156e1a7612df
Wealthyturtle <19924575+Whimsyturtle@users.noreply.github.com>
2025-05-31 11:59:55 +0800

    AddWarranty StyleSheet

--- frontend/app/add_warranty.tsx
+++ frontend/app/add_warranty.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, Button } from "react-native";
+import { StyleSheet, View, Text, TextInput, Button } from "react-native";
 import { useState } from 'react';
 import { DatePickerModal } from "react-native-paper-dates"; // Tried https://github.com/react-native-datetimepicker/datetimepicker and https://github.com/mmazzarolo/react-native-modal-datetime-picker, but both don't work on Web
 import { format } from 'date-fns';
@@ -15,7 +15,7 @@ export default function AddWarranty() {
     return format(date, 'yyyy-MM-dd'); // Ref: https://date-fns.org/docs/format
   };
 
-  const handleButton = async () => {
+  const handleSubmitButton = async () => {
     const data = {
       product_name: productName,
       product_categories: productCategories.split(','),
@@ -32,6 +32,22 @@ export default function AddWarranty() {
     console.log('Backend Response is', res.status, json);
   };
 
+  const styleSheet = StyleSheet.create({
+    textLabel: {
+      marginBottom: 5,
+    },
+    textInput: {
+      marginBottom: 15,
+      borderWidth: 1,
+    },
+    datePickerButtonViewWrapper: {
+      marginBottom: 15,
+    },
+    submitButtonViewWrapper: {
+      marginTop: 15,
+    },
+  });
+
   return (
     <View
       style={{
@@ -40,28 +56,28 @@ export default function AddWarranty() {
         alignItems: "center",
       }}
     >
-      <Text>Product Name:</Text>
-      <TextInput
+      <Text style={styleSheet.textLabel}>Product Name:</Text>
+      <TextInput style={styleSheet.textInput}
         value={productName}
         onChangeText={setProductName}
         placeholder="Laptop"
         placeholderTextColor="#808080"
-        style={{ borderWidth: 1 }}
       />
 
-      <Text>Product Categories (Comma-Separated):</Text>
-      <TextInput
+      <Text style={styleSheet.textLabel}>Product Categories (Comma-Separated):</Text>
+      <TextInput style={styleSheet.textInput}
         value={productCategories}
         onChangeText={setProductCategories}
         placeholder="Electonics,Software"
         placeholderTextColor="#808080"
-        style={{ borderWidth: 1 }}
       />
 
-      <Text>Purchase Date:</Text>
-      <Button title={formatDate(purchaseDate)} onPress={() => {
-        setOpenPurchaseDatePicker(true);
-      }}/>
+      <Text style={styleSheet.textLabel}>Purchase Date:</Text>
+      <View style={styleSheet.datePickerButtonViewWrapper}>
+        <Button title={formatDate(purchaseDate)} onPress={() => {
+          setOpenPurchaseDatePicker(true);
+        }}/>
+      </View>
       <DatePickerModal
           locale="en"
           mode="single"
@@ -77,10 +93,12 @@ export default function AddWarranty() {
           }}
       />
 
-      <Text>Expiration Date:</Text>
-      <Button title={formatDate(expirationDate)} onPress={() => {
-        setOpenExpirationDatePicker(true);
-      }}/>
+      <Text style={styleSheet.textLabel}>Expiration Date:</Text>
+      <View style={styleSheet.datePickerButtonViewWrapper}>
+        <Button title={formatDate(expirationDate)} onPress={() => {
+          setOpenExpirationDatePicker(true);
+        }}/>
+      </View>
       <DatePickerModal
           locale="en"
           mode="single"
@@ -96,7 +114,9 @@ export default function AddWarranty() {
           }}
       />
 
-      <Button title="Add Warranty" onPress={handleButton} />
+      <View style={styleSheet.submitButtonViewWrapper}>
+        <Button title="Add Warranty" onPress={handleSubmitButton} />
+      </View>
     </View>
   );
 }
